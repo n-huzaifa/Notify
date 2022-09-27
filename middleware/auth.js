3b739c44d309b6de25e4d2f73d9bf7c5363735f7
n-huzaifa <noorhuzaifa11@gmail.com>
2022-09-28 00:10:30 +0500

    JWT Auth Middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,11 +2,21 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 function authenticator(req, res, next) {
-  const token =
-    req.body.token || req.header["x-access-token"] || req.body.query;
-  //  TODO setup JWT Auth verification middleware
-  const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  if (decoded) return next();
+  try {
+    const token =
+      req.body.token || req.headers["x-access-token"] || req.body.query;
+
+    if (!token) {
+      res.status(403).json({ error: "JWT Token required" });
+      return;
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    return next();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 }
 
 module.exports = authenticator;
