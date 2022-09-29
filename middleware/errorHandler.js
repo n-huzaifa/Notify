857a6a64fac77205eab2814d93036ec0f1e29122
n-huzaifa <noorhuzaifa11@gmail.com>
2022-09-29 14:52:21 +0500

    next () omission in error handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,7 +2,6 @@ function ErrorHandler(error, req, res, next) {
   res
     .status(error.status || 500)
     .json({ error: error.message || "Internal Server Error" });
-  next();
 }
 
 module.exports = ErrorHandler;
